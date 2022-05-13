2b3df6f5767f2ca9b277f7f8c9f6c0c197600d3d
Agbleze <lagbleze@st.ug.edu.gh>
2022-05-14 00:02:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,6 @@ from dash import dcc
 from dash import Input, Output, State, callback
 import dash_bootstrap_components as dbc
 import dash_trich_components as dtc
-import 
-
-
 import pandas as pd
 import logging
 from urllib.parse import unquote
