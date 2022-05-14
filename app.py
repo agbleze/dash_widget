dc8af14c21ce04c837b099a4a52f0f8ed98be5f7
Agbleze <lagbleze@st.ug.edu.gh>
2022-05-14 17:38:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# %%
 import dash
 from dash import html
 from dash import dcc
@@ -7,3 +8,45 @@ import dash_trich_components as dtc
 import pandas as pd
 import logging
 from urllib.parse import unquote
+# %%
+card_icon = {
+    "color": "green",
+    "textAlign": "center",
+    "fontSize": "4em",
+    "margin": "auto",
+}
+
+class value_box:
+    def __init__(self, width='14em', 
+                 value_box_output_id='value_box_output_id',
+                 value_box_output_value='value output',
+                 value_box_desc_id='description of value',
+                 value_box_desc_value='desc value',
+                 value_background_color='yellow',
+                 value_icon_background_color='lightgreen',
+                 value_icon_color='green',
+                 value_icon_align='center'
+                 ):
+        self.value_box_output_id = value_box_output_id
+        self.width = width
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.CYBORG, dbc.icons.BOOTSTRAP, dbc.icons.FONT_AWESOME])
+
+app.layout = html.Div(style={'width': '14em'},
+                        children=[
+                            dbc.CardGroup([
+                                            dbc.Card([html.H4(children='Value output'),
+                                                      html.P(children='desc value')
+                                                        ]
+                                                     ),
+                                            dbc.Card(children=[html.Div(className='bi bi-bullseye',
+                                                                        style=card_icon
+                                                                )
+                                                                ]
+                                                     )
+                                        ])
+                        ]
+                )
+
+app.run_server(port=8080)
+# %%
